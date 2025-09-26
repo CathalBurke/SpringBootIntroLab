be0ea6943651df4ef61108734724500df64836d6
CATHALBURKE-STUDENT <g00413483@atu.ie>
2025-09-26 09:55:56 +0100

    Steps 1- 7 of the Lab Excersise completed

---
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -0,0 +1,36 @@
+package ie.atu.week2;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class RequestController {
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello";
+}
+
+// using a get request
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age) {
+        return "Name " + name + ", Age " + age;
+    }
+
+    @GetMapping("/person")
+    public Person getPerson(){
+        return new Person("Stephen", 25);
+    }
+
+
+
+
+    }
+
+
+
